0ce91d4eba7d7e666d35bdbf02784eb22e76919c
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2024-03-20 23:25:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6783196044"
+OWNER_ID = "7176027733"
 SUPPORT_GRP = "RUDRA_JAAT_BIO"
 UPDATE_CHNL = "RUDRA_JAAT_BIO"
-OWNER_USERNAME = "RUDRA_JAAT_1"
+OWNER_USERNAME = "RUDRA_JAAT_BIO"
 
